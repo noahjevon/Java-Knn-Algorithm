303488ed96f20ce540e69000b440587912ee0561
noahjevon <noahjevon@gmail.com>
2021-03-08 12:42:29 +0000

    Moved to packages III

--- src/cmet/ac/st20141224/Controller/ImageController.java
+++ src/cmet/ac/st20141224/Controller/ImageController.java
@@ -1,45 +1,38 @@
-package cmet.ac.st20141224;
+package cmet.ac.st20141224.Controller;
+
+import cmet.ac.st20141224.Model.Model;
+import cmet.ac.st20141224.View.Main;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
-import javax.swing.filechooser.*;
-import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileFilter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.io.FileFilter.*;
 
-public class Controller {
+public class ImageController {
     ArrayList<String> images = new ArrayList<>();  // Array to store image file names
     ArrayList<String> imagesPath = new ArrayList<>(); // Array to store image paths
     String imagesString = "";
-
-    ArrayList sources = new ArrayList();  // Array to store source file names
-    ArrayList sourcesPath = new ArrayList(); // Array to store source paths
-    String sourcesString = "";
 
     JFileChooser fileChooser;  // File chooser to allow selection
 
     Model model;
     Main view;
 
-    public Controller(Model model, Main view) {
+    public ImageController(Model model, Main view) {
         this.model = model;
         this.view = view;
 
         initController();
     }
 
-    private void initController() {
+    public void initController() {
         this.view.btnImage.addActionListener((ae) -> selectImage());
-        this.view.btnSrc.addActionListener((ae) -> selectSource());
-        this.view.btnRun.addActionListener((ae) -> runModel());
     }
 
-    private void selectImage() {
+    public void selectImage() {
         images.clear();  // Clearing arrays & string
         imagesPath.clear();
         imagesString = "";
@@ -88,37 +81,4 @@ public class Controller {
             this.view.jlabImg.setText(this.model.getInputtext());
         }
     }
-
-    private void selectSource() {
-        // Create a new JFrame container
-        JFrame jfrm = new JFrame("Select Source");
-
-        // Creating file filter to only show .bin files
-        this.fileChooser = new JFileChooser();
-        fileChooser.setMultiSelectionEnabled(true);  // Allow multiple selection
-        FileNameExtensionFilter filter = new FileNameExtensionFilter("BIN file", "bin");  // File filter
-        fileChooser.setFileFilter(filter);  // Apply file filter
-        int result = fileChooser.showOpenDialog(jfrm);  // Open form
-
-        if (result == JFileChooser.APPROVE_OPTION) {  // If file approved
-            File[] selectedFile = fileChooser.getSelectedFiles();
-
-            for (File file : selectedFile) {
-                String name = file.getName();  // Get source name, append to array + string
-                sources.add(name);
-                sourcesString += name + " ";
-
-                String path = file.getAbsolutePath();  // Get source path, append to array + string
-                sourcesPath.add(path);
-            }
-            this.model.setSourcetext(sourcesString);  // Updating labels
-            this.view.jlabSrc.setText(this.model.getInputtext());
-        }
-    }
-
-    private void runModel() {
-        // Make file input streams for data and images - loop through array, start new thread for each
-        System.out.println("Ran model.");
-    }
 }
-
