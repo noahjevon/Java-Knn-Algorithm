fd2c2b6ec48daab23359b25dde539cf7082f173a
noahjevon <noahjevon@gmail.com>
2021-03-14 21:34:14 +0000

    Added comments

--- src/cmet/ac/st20141224/FileIO/CheckParams.java
+++ src/cmet/ac/st20141224/FileIO/CheckParams.java
@@ -14,16 +14,14 @@ public class CheckParams {
     Boolean lbl;
     Boolean kVal;
 
-    String lblSrc;
-
 
     public CheckParams(Model model) {
-        this.model = model;
-
+        this.model = model;  //
     }
 
+
     public void setImgSrc(String imgSrc) {
-        if (imgSrc.equals("")) {
+        if (imgSrc.equals("")) {  // Check if image source is null
             ErrorView.errorMessage("Image source cannot be null!", "Image Source Error");
             setImg(false);
         } else {
@@ -31,9 +29,10 @@ public class CheckParams {
         }
     }
 
+
     public void setSrcSrc(String srcSrc) {
-        if (srcSrc.equals("")) {
-            ErrorView.errorMessage("Model source cannot be null!", "Model Source Error");
+        if (srcSrc.equals("")) {  // Check if training source is null
+            ErrorView.errorMessage("Training source cannot be null!", "Training Source Error");
             setSrc(false);
         } else {
             this.model.setSrcSrc(srcSrc);
@@ -41,8 +40,9 @@ public class CheckParams {
         }
     }
 
+
     public void setLblSrc(String lblSrc) {
-        if (lblSrc.equals("")) {
+        if (lblSrc.equals("")) {  // Check if training source is null
             ErrorView.errorMessage("Label source cannot be null!", "Label Source Error");
             setLbl(false);
         } else {
@@ -50,14 +50,15 @@ public class CheckParams {
         }
     }
 
+
     public void setkValue(String kValue) {
         try {
             int kValueInt = Integer.parseInt(kValue);
-            if (kValue == null) {
+            if (kValue == null) {  // Check if K value is null
                 ErrorView.errorMessage("K value cannot be null!", "K value Source Error");
                 setkVal(false);
             }
-            if ((kValueInt < 1) || (kValueInt > 10)) {
+            if ((kValueInt < 1) || (kValueInt > 10)) {  // Check if K value is between 1 and 10
                 ErrorView.errorMessage("K value must be between 1 and 10!", "K value Error");
                 setkVal(false);
             }
@@ -65,11 +66,45 @@ public class CheckParams {
                 this.model.setkValue(kValue);
                 setkVal(true);
             }
-        } catch (NumberFormatException e) {
+        } catch (NumberFormatException e) {  // Check K value is a number
             ErrorView.errorMessage("K value must be a number!", "K value Error");
         }
     }
 
+
+    public void checkParams() {  // Check to see if all boolean values return true
+        if ((this.getImg() == true) && (this.getSrc() == true) && (this.getLbl() == true) && (this.getkVal() == true)) {
+            AlertView.alertMessage("Running model!", "Alert");  // Inform user model is running. Tests passed
+
+            try {  // Read specified label
+                this.model.getLabelReader().setFilename(this.model.getLblSrc());
+                this.model.getLabelReader().read();
+            } catch (IOException e) {  // Inform user there was an error reading the label
+                ErrorView.errorMessage("Error reading label data", "Label Error");
+            }
+
+            try {  // Read specified source
+                this.model.getImageReader().setFilename(this.model.getImgSrc());
+                this.model.getImageReader().read();
+            } catch (IOException e) {  // Inform user there was an error reading the source
+                ErrorView.errorMessage("Error reading source data", "Source Error");
+            }
+
+            try {  // Read specified image
+                this.model.getSourceReader().setFilename(this.model.getSrcSrc());
+                this.model.getSourceReader().setLabelName(this.model.getLblSrc());
+                this.model.getSourceReader().read();
+            } catch (IOException e) {  // Inform user there was an error reading the source
+                ErrorView.errorMessage("Error reading image data", "Image Error");
+            }
+
+        }
+        else {  // Let user know the parameters were invalid
+            ErrorView.errorMessage("Please check parameters and try again.", "Run Error");
+        }
+    }
+
+    // Setters & Getters
     public Boolean getImg() {
         return img;
     }
@@ -102,35 +137,4 @@ public class CheckParams {
         this.kVal = kVal;
     }
 
-    public void checkParams() {
-        if ((this.getImg() == true) && (this.getSrc() == true) && (this.getLbl() == true) && (this.getkVal() == true)) {
-            AlertView.alertMessage("Running model!", "Alert");
-
-            try {  // Label reader
-                this.model.getLabelReader().setFilename(this.model.getLblSrc());
-                this.model.getLabelReader().read();
-            } catch (IOException e) {
-                ErrorView.errorMessage("Error reading label data", "Label Error");
-            }
-
-            try {  // Source reader
-                this.model.getImageReader().setFilename(this.model.getImgSrc());
-                this.model.getImageReader().read();
-            } catch (IOException e) {
-                ErrorView.errorMessage("Error reading image data", "Image Error");
-            }
-
-            try {  // Image reader
-                this.model.getSourceReader().setFilename(this.model.getSrcSrc());
-                this.model.getSourceReader().setLabelName(this.model.getLblSrc());
-                this.model.getSourceReader().read();
-            } catch (IOException e) {
-                ErrorView.errorMessage("Error reading image data", "Image Error");
-            }
-
-        }
-        else {
-            ErrorView.errorMessage("Please check parameters and try again.", "Run Error");
-        }
-    }
 }
