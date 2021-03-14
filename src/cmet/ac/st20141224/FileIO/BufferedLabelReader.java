74a6cc1f2dc645151d411b85b6eecd332b9402c6
noahjevon <noahjevon@gmail.com>
2021-03-14 18:59:54 +0000

    Added ability to read label .txt file. Created an array to store each label.

--- src/cmet/ac/st20141224/FileIO/BufferedLabelReader.java
+++ src/cmet/ac/st20141224/FileIO/BufferedLabelReader.java
@@ -1,11 +1,28 @@
 package cmet.ac.st20141224.FileIO;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class BufferedLabelReader implements IFileReader {
+
+    String filePath;
+    List<String> labelList;
+
+    public BufferedLabelReader() {
+        this.labelList = new ArrayList<String>();
+    }
+
     @Override
     public void read() throws IOException {
-
+        labelList.clear();
+        Scanner s = new Scanner(new File(filePath));
+        while (s.hasNext()) {
+            labelList.add(s.next());
+        }
+        s.close();
+        System.out.println(labelList);
     }
 
     @Override
@@ -15,11 +32,20 @@ public class BufferedLabelReader implements IFileReader {
 
     @Override
     public void setFilename(String filename) {
-
+        this.filePath = filename;
     }
 
     @Override
     public String getFilename() {
         return null;
+    }
+
+    @Override
+    public void setLabelName(String labelName) {
+
+    }
+
+    public List getLabels() {
+        return this.labelList;
     }
 }
