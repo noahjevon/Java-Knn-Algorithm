708a534a53e6b17bf39262ab41e2d0e3fbbbc201
noahjevon <noahjevon@gmail.com>
2021-03-15 19:20:00 +0000

    Added binary reading for test image

--- src/cmet/ac/st20141224/FileIO/BufferedImageReader.java
+++ src/cmet/ac/st20141224/FileIO/BufferedImageReader.java
@@ -16,7 +16,7 @@ import java.util.Scanner;
 
 
 public class BufferedImageReader implements IFileReader {
-    String CIFAR;
+    String testImage;
     String labelPath;
 
     FileInputStream in_stream_images;
@@ -26,10 +26,52 @@ public class BufferedImageReader implements IFileReader {
 
     String fileName;
     BufferedImage img;
-    ArrayList<String> images = new ArrayList<>();
+
+    public BufferedImageReader() {
+        this.labelList = new ArrayList<>();
+    }
 
     @Override
     public void read() throws IOException {
+        String image_filename = testImage;
+
+        in_stream_images = new FileInputStream(image_filename);
+
+        // Read list of labels
+//        labelList.clear();
+//        Scanner s = new Scanner(new File(labelPath));
+//        while (s.hasNext()) {
+//            labelList.add(s.next());
+//        }
+//        s.close();
+
+        while(in_stream_images.available() > 0) {
+
+            byte[] red_Data = new byte[1024];
+            in_stream_images.read(red_Data);
+
+            byte[] green_Data = new byte[1024];
+            in_stream_images.read(green_Data);
+
+            byte[] blue_Data = new byte[1024];
+            in_stream_images.read(blue_Data);
+
+            BufferedImage img = new BufferedImage(32, 32, BufferedImage.TYPE_INT_ARGB);
+
+            for (int i = 0; i < 32; i++) {
+                for (int j = 0; j < 32; j++) {
+                    Color color = new Color(
+                            red_Data[i * 32 + j] & 0xFF,
+                            green_Data[i * 32 + j] & 0xFF,
+                            blue_Data[i * 32 + j] & 0xFF);
+                    img.setRGB(i, j, color.getRGB());
+                    int red = red_Data[i * 32 + j] & 0xFF;
+                    int green = green_Data[i * 32 + j] & 0xFF;
+                    int blue = blue_Data[i * 32 + j] & 0xFF;
+                    System.out.println(color);
+                }
+            }
+        }
     }
 
     @Override
@@ -39,16 +81,16 @@ public class BufferedImageReader implements IFileReader {
 
     @Override
     public void setFilename(String filename) {
-        this.fileName = filename;
+        this.testImage = filename;
     }
 
     @Override
     public String getFilename() {
-        return this.fileName;
+        return this.testImage;
     }
 
     @Override
     public void setLabelName(String labelName) {
-
+        this.labelPath = labelName;
     }
 }
