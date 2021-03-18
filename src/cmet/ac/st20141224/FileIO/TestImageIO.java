9846e3b9440403cffb9b61c16daa3ebd8ffae409
noahjevon <noahjevon@gmail.com>
2021-03-18 17:20:10 +0000

    Added basic error message

--- src/cmet/ac/st20141224/FileIO/TestImageIO.java
+++ src/cmet/ac/st20141224/FileIO/TestImageIO.java
@@ -37,7 +37,7 @@ public class TestImageIO implements IFileReader {
         this.filePath = new ArrayList<>();
     }
 
-    
+
     @Override
     public void read() throws IOException {
         File file = new File(testImage); // Path of directory or file
@@ -68,66 +68,71 @@ public class TestImageIO implements IFileReader {
     }
 
     public void readData(String path) throws IOException {
-        FileInputStream imageStream; // Declaring new FileInputStream to read file
-        String fileName = path; // Filepath
+        try {
+            FileInputStream imageStream; // Declaring new FileInputStream to read file
+            String fileName = path; // Filepath
 
-        File path2 = new File(fileName); // Getting whole path of the specified file
-        String name = path2.getName(); // Getting just the name of the specified file
-        System.out.println(name);
-        String labelStr = Character.toString(name.charAt(0)); // Getting the character at the first index in the file name
-        this.label = Integer.parseInt(labelStr); // Converting this character to integer value
+            File path2 = new File(fileName); // Getting whole path of the specified file
+            String name = path2.getName(); // Getting just the name of the specified file
+            System.out.println(name);
+            String labelStr = Character.toString(name.charAt(0)); // Getting the character at the first index in the file name
+            this.label = Integer.parseInt(labelStr); // Converting this character to integer value
 
-        imageStream = new FileInputStream(fileName); // Declaring new FileInputStream to read file
-        while(imageStream.available() > 0) { // Checking that image data is available
+            imageStream = new FileInputStream(fileName); // Declaring new FileInputStream to read file
+            while(imageStream.available() > 0) { // Checking that image data is available
 
-            byte[] red_Data = new byte[1024]; // Reading 1024 bytes of red data
-            imageStream.read(red_Data);
+                byte[] red_Data = new byte[1024]; // Reading 1024 bytes of red data
+                imageStream.read(red_Data);
 
-            byte[] green_Data = new byte[1024]; // Reading 1024 bytes of green data
-            imageStream.read(green_Data);
+                byte[] green_Data = new byte[1024]; // Reading 1024 bytes of green data
+                imageStream.read(green_Data);
 
-            byte[] blue_Data = new byte[1024]; // Reading 1024 bytes of blue data
-            imageStream.read(blue_Data);
+                byte[] blue_Data = new byte[1024]; // Reading 1024 bytes of blue data
+                imageStream.read(blue_Data);
 
-            BufferedImage img = new BufferedImage // New BufferedImage for the image being read
-                    (32, 32, BufferedImage.TYPE_INT_ARGB);
+                BufferedImage img = new BufferedImage // New BufferedImage for the image being read
+                        (32, 32, BufferedImage.TYPE_INT_ARGB);
 
-            this.redList = new ArrayList<>(); // Initialising list to store red data
-            this.greenList = new ArrayList<>(); // Initialising list to store green data
-            this.blueList = new ArrayList<>(); // Initialising list to store blue data
-            this.greyscaleList = new ArrayList<>(); // Initialising list to store greyscale data
+                int image_width = img.getWidth(); // Getting the image width
+                int image_height = img.getHeight(); // Getting the image height
+                if (image_height > 32 && image_width > 32) { // Checking that image is 32 x 32 or less
+                    ErrorView.errorMessage("Image cannot be greater than 32x32!", "Image Size Error");
+                } else {
 
-            int image_width = img.getWidth(); // Getting the image width
-            int image_height = img.getHeight(); // Getting the image height
-            if (image_height > 32 && image_width > 32) { // Checking that image is 32 x 32 or less
-                ErrorView.errorMessage("Image cannot be greater than 32x32!", "Image Size Error");
-            } else {
+                    this.redList = new ArrayList<>(); // Initialising list to store red data
+                    this.greenList = new ArrayList<>(); // Initialising list to store green data
+                    this.blueList = new ArrayList<>(); // Initialising list to store blue data
+                    this.greyscaleList = new ArrayList<>(); // Initialising list to store greyscale data
 
-                for (int i = 0; i < 32; i++) { // Loop each pixel in the image
-                    for (int j = 0; j < 32; j++) {
-                        Color color = new Color(
-                                red_Data[i * 32 + j] & 0xFF, // Get red data from pixel
-                                green_Data[i * 32 + j] & 0xFF, // get green data from pixel
-                                blue_Data[i * 32 + j] & 0xFF); // Get blue data from pixel
-                        img.setRGB(i, j, color.getRGB()); // Getting the RGB value of the pixel
+                    for (int i = 0; i < 32; i++) { // Loop each pixel in the image
+                        for (int j = 0; j < 32; j++) {
+                            Color color = new Color(
+                                    red_Data[i * 32 + j] & 0xFF, // Get red data from pixel
+                                    green_Data[i * 32 + j] & 0xFF, // get green data from pixel
+                                    blue_Data[i * 32 + j] & 0xFF); // Get blue data from pixel
+                            img.setRGB(i, j, color.getRGB()); // Getting the RGB value of the pixel
 
-                        int red = red_Data[i * 32 + j] & 0xFF; // Storing red data as int
-                        int green = green_Data[i * 32 + j] & 0xFF; // Storing green data as int
-                        int blue = blue_Data[i * 32 + j] & 0xFF; // Storing blue data as int
+                            int red = red_Data[i * 32 + j] & 0xFF; // Storing red data as int
+                            int green = green_Data[i * 32 + j] & 0xFF; // Storing green data as int
+                            int blue = blue_Data[i * 32 + j] & 0xFF; // Storing blue data as int
 
-                        int greyscale = (int) ((0.3 * red) + // Formula to calculate greyscale value of the pixel
-                                (0.59 * green) + (0.11 * blue));
+                            int greyscale = (int) ((0.3 * red) + // Formula to calculate greyscale value of the pixel
+                                    (0.59 * green) + (0.11 * blue));
 
-                        this.redList.add(red); // Adding red pixel data to red list
-                        this.greenList.add(green); // Adding green pixel data to green list
-                        this.blueList.add(blue); // Adding blue pixel data to blue list
-                        this.greyscaleList.add(greyscale); // Adding greyscale pixel data to greyscale list
+                            this.redList.add(red); // Adding red pixel data to red list
+                            this.greenList.add(green); // Adding green pixel data to green list
+                            this.blueList.add(blue); // Adding blue pixel data to blue list
+                            this.greyscaleList.add(greyscale); // Adding greyscale pixel data to greyscale list
+                        }
                     }
                 }
             }
+            this.imageList.add(new TestImageModel // Creating new object containing all image data
+                    (fileName, label, redList, greenList, blueList, greyscaleList));
+        } catch (NumberFormatException e) {
+            ErrorView.errorMessage("Error reading image data. Check if incorrect image is" +
+                    "present in directory and try again.", "Image Read Error");
         }
-        this.imageList.add(new TestImageModel // Creating new object containing all image data
-                (fileName, label, redList, greenList, blueList, greyscaleList));
     }
 
     // Getters & setters
