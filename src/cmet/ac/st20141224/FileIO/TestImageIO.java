d9e62be7d52999b2d236fb86ddab27f5fe977bcd
noahjevon <noahjevon@gmail.com>
2021-03-28 17:03:06 +0100

    Added Lambda expression to TEstImageIO class

--- src/cmet/ac/st20141224/FileIO/TestImageIO.java
+++ src/cmet/ac/st20141224/FileIO/TestImageIO.java
@@ -49,9 +49,13 @@ public class TestImageIO implements IFileReader {
             Files.walk(dir).forEach(path -> showFile(path.toFile())); // Run toFile to add paths to array
             filePath.remove(0); // Remove first index (It's the directory itself)
 
-            for (String path : filePath) { // Start reading each file
-                readData(path);
-            }
+            filePath.forEach( (path) -> { // Start reading each file
+                try {
+                    readData(path); // Run readData function with path as argument
+                } catch (IOException e) {
+                    e.printStackTrace(); // Throw exception
+                }
+            });
 
         } else { // If not a directory, read single file
             readData(testImage);
