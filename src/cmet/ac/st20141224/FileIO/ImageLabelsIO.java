2f6ce078d43e5977d97950d00382542efb47f011
noahjevon <noahjevon@gmail.com>
2021-03-31 18:50:41 +0100

    Added if statement to check for the filepath of the label input. If it is not a .txt file, error message displays.

--- src/cmet/ac/st20141224/FileIO/ImageLabelsIO.java
+++ src/cmet/ac/st20141224/FileIO/ImageLabelsIO.java
@@ -1,14 +1,17 @@
 package cmet.ac.st20141224.FileIO;
 
 import cmet.ac.st20141224.Model.ImageLabelModel;
+import cmet.ac.st20141224.View.ErrorView;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ImageLabelsIO implements IFileReader {
 
     String filePath; // String variable for the file path of the image labels
+    String extension; // String variable for the extension of the file
     List<ImageLabelModel> labelList; // List to store the labels
 
     public ImageLabelsIO() {
@@ -17,15 +20,29 @@ public class ImageLabelsIO implements IFileReader {
 
     @Override
     public void read() throws IOException {
+
+        // Get the file extension
+        int index = this.filePath.lastIndexOf('.');
+        if(index > 0) {
+            this.extension = this.filePath.substring(index + 1);
+        }
+
+        //Read the file
         String line = "";
-        if(this.filePath != null); // Checking to see if location is null
-            try(BufferedReader br = new BufferedReader(new FileReader(this.filePath))) { // Buffered reader to read file
-                while((line = br.readLine()) != null) { // While the line in the file is not null, read the file
-                    String label = line; // Assigning line in file to String
-                    this.labelList.add(new ImageLabelModel(label)); // Adding String to list that stores labels
+            if(this.extension.equals("txt")) {
+                if(this.filePath != null); // Checking to see if location is null
+                try(BufferedReader br = new BufferedReader(new FileReader(this.filePath))) { // Buffered reader to read file
+                    while((line = br.readLine()) != null) { // While the line in the file is not null, read the file
+                        String label = line; // Assigning line in file to String
+                        this.labelList.add(new ImageLabelModel(label)); // Adding String to list that stores labels
+                    }
                 }
             }
+            else {
+                ErrorView.errorMessage("Label file: " + this.filePath + " not .txt. Please try again", "Label Error");
+            }
     }
+
 
     /**
      * Getters & setters
